b20b22aaaa63371e7b07805bdd7dbf7e7f6f95b6
yodabytz <yodabytz@users.noreply.github.com>
2026-02-18 18:00:21 -0500

    Update version to 1.0.4

--- version.php
+++ version.php
@@ -4,11 +4,11 @@
  * This file is automatically updated during the update process
  */
 return [
-    'version' => '1.2.0',
+    'version' => '1.0.4',
     'version_major' => 1,
-    'version_minor' => 2,
-    'version_patch' => 0,
-    'release_date' => '2026-01-15',
+    'version_minor' => 0,
+    'version_patch' => 4,
+    'release_date' => '2026-02-18',
     'product' => 'Apparix E-Commerce Platform',
     'update_server' => 'https://apparix.vibrixmedia.com'
 ];
