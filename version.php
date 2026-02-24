9abe54dface186bbe9a1c984bbc4fb827eb2f692
yodabytz <yodabytz@users.noreply.github.com>
2026-02-24 13:51:27 -0500

    Bump version to 1.2.2

--- version.php
+++ version.php
@@ -4,11 +4,11 @@
  * This file is automatically updated during the update process
  */
 return [
-    'version' => '1.2.1',
+    'version' => '1.2.2',
     'version_major' => 1,
     'version_minor' => 2,
-    'version_patch' => 1,
-    'release_date' => '2026-02-22',
+    'version_patch' => 2,
+    'release_date' => '2026-02-24',
     'product' => 'Apparix E-Commerce Platform',
     'update_server' => 'https://apparix.app'
 ];
